f2f51210dbc91af5657c3e8c60889f1b8681e38c
AfrikanMeals <admin@afrikan-meals.com>
2026-09-09 23:28:14 +0100

    refactor(jwt): update method signatures to use nullable types for optional parameters
    Modify the CachedKeySet, JWK, and JWT classes to allow nullable types for optional parameters in method signatures. This change enhances type safety and clarity in the codebase, ensuring that null values are explicitly handled where applicable.

--- modules/products/vendor/firebase/php-jwt/src/JWK.php
+++ modules/products/vendor/firebase/php-jwt/src/JWK.php
@@ -46,7 +46,7 @@ class JWK
      *
      * @uses parseKey
      */
-    public static function parseKeySet(array $jwks, string $defaultAlg = null): array
+    public static function parseKeySet(array $jwks, ?string $defaultAlg = null): array
     {
         $keys = [];
 
@@ -87,7 +87,7 @@ class JWK
      *
      * @uses createPemFromModulusAndExponent
      */
-    public static function parseKey(array $jwk, string $defaultAlg = null): ?Key
+    public static function parseKey(array $jwk, ?string $defaultAlg = null): ?Key
     {
         if (empty($jwk)) {
             throw new InvalidArgumentException('JWK must not be empty');
